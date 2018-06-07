ddb1e2169a2f09d1f93cd9cdfd56783bb9896164
Nakayama <akiraichi5430@gmail.com>
2018-06-08 00:19:43 +0900

    6月8日0時19分

---
+++ TodoAppApp/TodoTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TodoTableViewCell.swift
+//  TodoAppApp
+//
+//  Created by 中山昌勲 on 2018/06/07.
+//  Copyright © 2018年 Aki. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var todoTextCell: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
