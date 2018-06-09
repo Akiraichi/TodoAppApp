e3bb7ed4fc187d8a4d396165c326e5c61ddb72a4
Nakayama <akiraichi5430@gmail.com>
2018-06-09 10:22:52 +0900

    チェックマークありで、取り消し線を文字に入れる機能を実装（前回のコミット）。 それに加えて、チェックマークありで、文字と取り消し線を灰色にする機能を実装

--- TodoAppApp/TodoTableViewCell.swift
+++ TodoAppApp/TodoTableViewCell.swift
@@ -19,13 +19,18 @@ class TodoTableViewCell: UITableViewCell {
             let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: todoTextCell.text!)
             attributeString.addAttribute(NSAttributedStringKey.strikethroughStyle, value: 2, range: NSMakeRange(0, attributeString.length))
             self.todoTextCell.attributedText=attributeString
+            
+            //textcolorを変更する
+            todoTextCell.textColor=UIColor(hex: "ccd1d2")
             //取り消し線を加える
             strikeOn=true
         }else{
             let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: todoTextCell.text!)
             attributeString.addAttribute(NSAttributedStringKey.strikethroughStyle, value: 0, range: NSMakeRange(0, attributeString.length))
             self.todoTextCell.attributedText=attributeString
-            
+           
+            //textColorを戻す
+            todoTextCell.textColor=UIColor(hex: "000000")
             //取り消し線を消す
             strikeOn=false
         }
@@ -41,4 +46,19 @@ class TodoTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+//色を変更しやすくする
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat) {
+        let v = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
+        let r = CGFloat(Int(v[0] + v[1], radix: 16) ?? 0) / 255.0
+        let g = CGFloat(Int(v[2] + v[3], radix: 16) ?? 0) / 255.0
+        let b = CGFloat(Int(v[4] + v[5], radix: 16) ?? 0) / 255.0
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+    
+    convenience init(hex: String) {
+        self.init(hex: hex, alpha: 1.0)
+    }
 }
