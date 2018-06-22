a92a8a8cdba142d9d49433b0ad20476014e5cd54
Nakayama <akiraichi5430@gmail.com>
2018-06-22 16:13:46 +0900

    Todoリストでナビゲーションバーに＜を入れた

--- TodoAppApp/AppDelegate.swift
+++ TodoAppApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         //ナビゲーションバーの＜を消す
-        UINavigationBar.appearance().backIndicatorImage = UIImage()
+        //UINavigationBar.appearance().backIndicatorImage = UIImage()
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage()
         //Nvigationバーのテキストカラーを白色に変更
         UINavigationBar.appearance().tintColor = UIColor.white
